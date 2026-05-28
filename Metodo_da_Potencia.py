dff1d383659aee87ee13b24bb79ace8321a5762a
Dr-Verdin <piscioneri24122004@gmail.com>
2026-05-28 09:08:01 -0300

    metodo da potencia

---
+++ Metodo_da_Potencia.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+k = int(input("Digite o número máximo de iterações: "));
+n = int(input("Digite a ordem da matriz: "));
+
+q0 = np.array(
+    list(map(float, input("Digite o vetor q0: ").split()))
+).reshape(n, 1)
+
+print("Vetor q0:")
+print(q0)
+
+if np.linalg.norm(q0, 2) != 1:
+    print("O vetor inicial q0 não é unitário.")
+
+A = []
+for i in range(n):
+    row = list(map(float, input(f"Digite a linha {i} da matriz A: ").split()))
+    A.append(row)
+
+A = np.array(A)
+
+print("Matriz A:")
+print(A)
+
+q_new = q0
+for k in range(k + 1):
+    print(f"=== Iteração {k}: ===")
+
+    w = A @ q_new
+    print("\nVetor w:")
+    print(w)
+
+    s = np.linalg.norm(w, 2)
+    print("\nValor de s:")
+    print(s)
+
+    q_new = w / s
+    print(f"\nVetor q_{k+1}:")
+    print(q_new)
+
+    autovalor_new = q_new.T @ A @ q_new
+    print(f"\nAutovalor aproximado λ_{k+1}:")
+    print(autovalor_new)
